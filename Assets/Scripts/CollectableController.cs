eeef3c851a59f5b66bc0802ac0c856caab6389ba
Saharsh11 <bsaharsh85@gmail.com>
2019-11-19 02:42:12 -0500

    Version 1.2.1

---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 offset;
+    private Vector3 offset1;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player one");
+        offset = transform.position - player.transform.position;
+        offset1 = transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 moving = new Vector3(90.0f, 0.0f, 0.0f);
+        transform.Rotate(Vector3.up * Time.deltaTime*speed);
+        
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate() 
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moving = new Vector3(horizontal, 0.0f, vertical);
+
+        /*transform.position += moving * Time.deltaTime;*/
+
+        rb.AddForce (moving * speed);
+    }
+}
